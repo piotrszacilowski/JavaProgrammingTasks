88583a39b961929c444753bff9d9ea7926165bcb
piotrszacilowski <szacilowski.piotr@gmail.com>
2020-02-18 02:59:09 +0100

    Created employee fields, constructor, getters and setters

--- src/objectProgramming1/PaymentCalculator/Employee.java
+++ src/objectProgramming1/PaymentCalculator/Employee.java
@@ -1,4 +1,37 @@
 package objectProgramming1.PaymentCalculator;
 
 public class Employee {
+    private String firstName;
+    private String lastName;
+    private double monthlySalary;
+
+    public Employee(String firstName, String lastName, double monthlySalary) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.monthlySalary = monthlySalary;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public double getMonthlySalary() {
+        return monthlySalary;
+    }
+
+    public void setMonthlySalary(double monthlySalary) {
+        this.monthlySalary = monthlySalary;
+    }
 }
