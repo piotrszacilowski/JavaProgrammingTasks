16434bf278c5a02672f6bf6376b8baa7cb57f189
piotrszacilowski <szacilowski.piotr@gmail.com>
2020-02-18 02:54:15 +0100

    Cabrio task done

--- src/objectProgramming1/Cabrio.java
+++ src/objectProgramming1/Cabrio.java
@@ -4,6 +4,49 @@ public class Cabrio {
     private String brand;
     private String model;
     private boolean move;
-    private boolean roofIsOpen;
-    
+    private boolean roofOpen;
+
+    public Cabrio(String brand, String model, boolean move) {
+        this.brand = brand;
+        this.model = model;
+        this.move = move;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public boolean isMove() {
+        return move;
+    }
+
+    public void setMove(boolean move) {
+        this.move = move;
+    }
+
+    public boolean isRoofOpen() {
+        return roofOpen;
+    }
+
+    public boolean setRoofOpen(boolean roofOpen) {
+        if (move == true) {
+            System.out.println("The roof cannot be opened while driving !");
+            return false;
+        } else {
+            this.roofOpen = roofOpen;
+        }
+        return false;
+    }
 }
---
+++ src/objectProgramming1/CabrioTest.java
@@ -0,0 +1,9 @@
+package objectProgramming1;
+
+public class CabrioTest {
+    public static void main(String[] args) {
+        Cabrio cabrio = new Cabrio("Mazda","MX-5",true);
+        cabrio.setRoofOpen(true);
+        System.out.println(cabrio.isRoofOpen());
+    }
+}
