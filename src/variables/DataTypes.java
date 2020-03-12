3acf5ef0629a834f55e4cb5ab862c0bc63e5354d
Piotr Szacilowski <szacilowski.piotr@gmail.com>
2020-03-12 14:08:24 +0100

    Data Types with widening conversion done

---
+++ src/variables/DataTypes.java
@@ -0,0 +1,10 @@
+package variables;
+
+public class DataTypes {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 4;
+        double div = (double)x / y;
+        System.out.println(div);
+    }
+}
