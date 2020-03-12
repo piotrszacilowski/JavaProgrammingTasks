a79f37872c8d7ca10ed8ea11fb464a5601afae5d
Piotr Szacilowski <szacilowski.piotr@gmail.com>
2020-03-12 19:46:45 +0100

    NumbersArgs class update

--- src/variables/NumberArgs.java
+++ src/variables/NumberArgs.java
@@ -8,15 +8,32 @@ public class NumberArgs {
 //        }
 //        System.out.println("Liczba argumentów: " + args.length);
 
-        int[] numbers = {1, 2, 3, 4, 5};
-        printSquares(numbers);
+        int[] numbers = {2, 4, 6, 8, 10};
+//        System.out.println("Liczba argumentów: " + numbers.length);
+//        printNumbers(numbers);
+//        printSquares(numbers);
+        printSum(numbers);
+    }
+
+    private static void printNumbers(int[] numbers) {
+        for (int number : numbers) {
+            System.out.print(number + ", ");
+        }
+        System.out.println();
     }
 
     private static void printSquares(int[] numbers) {
         for (int number : numbers) {
-            System.out.println(Math.pow(number, 2) + " ");
+            System.out.print(Math.pow(number, 2) + ", ");
         }
+        System.out.println();
     }
 
-    
+    private static void printSum(int[] numbers) {
+        int sum = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            sum =+ numbers[i];
+            System.out.println(sum);
+        }
+    }
 }
